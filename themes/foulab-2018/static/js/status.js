4bfdcac27c31bc2330026f72e7c29e9a5b89b160
Éric-Sébastien Lachance <eslachance@gmail.com>
2024-12-04 09:06:35 -0500

    restore inline loadical, remove mattermost, make status load relatively

--- themes/foulab-2018/static/js/status.js
+++ themes/foulab-2018/static/js/status.js
@@ -1,6 +1,6 @@
 // Fetch the status of the lab from the server and display it.
 // Thank you modern javascript for making this so easy!
-fetch("https://foulab.org/status/")
+fetch("/status/")
     .then(response => response.json())
     .then(data => {
         if (data["status"] === "OPEN") {
